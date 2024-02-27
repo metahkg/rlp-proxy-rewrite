11a6fb9cc7cb80f955e5c6e548e54a0d5dfb61b4
wcyat <wcyat@wcyat.me>
2024-02-27 21:55:21 +0800

    feat: add more metascraper rules; chore: update dependencies

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,8 +1,8 @@
 import puppeteer from "puppeteer-extra";
 import {
-  Browser,
-  DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
-  executablePath,
+    Browser,
+    DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+    executablePath,
 } from "puppeteer";
 import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
@@ -15,92 +15,110 @@ import metaScraperTitle from "metascraper-title";
 import metaScraperClearbit from "metascraper-clearbit";
 import metaScraperUrl from "metascraper-url";
 import metaScraperPublisher from "metascraper-publisher";
+import metaScraperManifest from "metascraper-manifest";
+import metaScraperAmazon from "metascraper-amazon";
+import metaScraperInstagram from "metascraper-instagram";
+import metaScraperSoundCloud from "metascraper-soundcloud";
+import metaScraperTelegram from "metascraper-telegram";
+import metaScraperUOL from "metascraper-uol";
+import metaScraperSpotify from "metascraper-spotify";
+import metaScraperTwitter from "metascraper-twitter";
+import metaScraperYoutube from "metascraper-youtube";
 import metaScraper from "metascraper";
 
 puppeteer.use(
-  AdblockerPlugin({
-    blockTrackers: true,
-    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
-  })
+    AdblockerPlugin({
+        blockTrackers: true,
+        interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+    }),
 );
 puppeteer.use(StealthPlugin());
 
 let browser: Browser;
 
 export async function initBrowser() {
-  browser = await puppeteer.launch({
-    headless: JSON.parse(process.env.NO_HEADLESS || "false") ? false : "new",
-    executablePath: executablePath(),
-    args: ["--no-sandbox"],
-  });
+    browser = await puppeteer.launch({
+        headless: JSON.parse(process.env.NO_HEADLESS || "false") ? false : true,
+        executablePath: executablePath(),
+        args: ["--no-sandbox"],
+    });
 }
 
 async function getContent(url: string): Promise<string | null> {
-  // if browser crashed / closed
-  if (!browser) {
-    await initBrowser();
-  }
-
-  while ((await browser.pages()).length > 20) {
-    await new Promise((resolve) => setTimeout(resolve, 200));
-  }
-
-  const page = await browser.newPage();
-
-  try {
-    await page.goto(url, { waitUntil: "networkidle0", timeout: 4000 });
-  } finally {
-    try {
-      const html = await page.evaluate(
-        () => document.querySelector("*").outerHTML
-      );
-      setTimeout(async () => {
-        try {
-          await page.close();
-        } catch {
-          // do nothing if failed
-        }
-      });
-      return html;
-    } catch {
-      try {
-        setTimeout(async () => {
-          try {
-            await page.close();
-          } catch {
-            // do nothing if failed
-          }
-        });
-      } finally {
-        return null;
-      }
+    // if browser crashed / closed
+    if (!browser) {
+        await initBrowser();
     }
-  }
+
+    while ((await browser.pages()).length > 20) {
+        await new Promise((resolve) => setTimeout(resolve, 200));
+    }
+
+    const page = await browser.newPage();
+
+    try {
+        await page.goto(url, { waitUntil: "networkidle0", timeout: 4000 });
+    } finally {
+        try {
+            const html = await page.evaluate(() => document.querySelector("*").outerHTML);
+            setTimeout(async () => {
+                try {
+                    await page.close();
+                } catch {
+                    // do nothing if failed
+                }
+            });
+            return html;
+        } catch {
+            try {
+                setTimeout(async () => {
+                    try {
+                        await page.close();
+                    } catch {
+                        // do nothing if failed
+                    }
+                });
+            } finally {
+                return null;
+            }
+        }
+    }
 }
 
 const metascraper = metaScraper([
-  metaScraperAuthor(),
-  metascraperDate(),
-  metaScraperDescription(),
-  metaScraperAuthor(),
-  metaScraperLogo(),
-  metaScraperClearbit(),
-  metaScraperPublisher(),
-  metaScraperTitle(),
-  metaScraperUrl(),
-  metaScraperImage(),
+    metaScraperAmazon(),
+    metaScraperClearbit(),
+    metaScraperInstagram(),
+    metaScraperSoundCloud(),
+    metaScraperSoundCloud(),
+    metaScraperTelegram(),
+    metaScraperUOL(),
+    metaScraperSpotify(),
+    metaScraperTwitter(),
+    metaScraperYoutube(),
+    metaScraperAuthor(),
+    metascraperDate(),
+    metaScraperDescription(),
+    metaScraperAuthor(),
+    metaScraperLogo(),
+    metaScraperClearbit(),
+    metaScraperPublisher(),
+    metaScraperTitle(),
+    metaScraperUrl(),
+    metaScraperImage(),
+    metaScraperManifest(),
 ]);
 
 export default async function metadataScraper(url: string) {
-  try {
-    const html = await getContent(url);
-    if (!html) return null;
-    const metadata = await metascraper({
-      url,
-      html,
-    });
-    return metadata;
-  } catch {
-    return null;
-  }
+    try {
+        const html = await getContent(url);
+        if (!html) return null;
+        const metadata = await metascraper({
+            url,
+            html,
+        });
+        return metadata;
+    } catch {
+        return null;
+    }
 }
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,8 @@
 import {
-  FastifyInstance,
-  FastifyPluginOptions,
-  FastifyReply,
-  FastifyRequest,
+    FastifyInstance,
+    FastifyPluginOptions,
+    FastifyReply,
+    FastifyRequest,
 } from "fastify";
 import { Type, Static } from "@sinclair/typebox";
 import { cacheCl } from "../lib/mongodb";
@@ -19,215 +19,217 @@ import isLocalhost from "is-localhost-ip";
 import { config } from "../lib/config";
 
 export default function (
-  fastify: FastifyInstance,
-  _opts: FastifyPluginOptions,
-  done: (err?: Error) => void
+    fastify: FastifyInstance,
+    _opts: FastifyPluginOptions,
+    done: (err?: Error) => void,
 ) {
-  const querySchema = Type.Object({
-    url: Type.String({
-      pattern:
-        "^https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]{0,2000})$",
-      maxLength: 1000,
-    }),
-    ...(config.HMAC_VERIFY &&
-      config.HMAC_KEY && {
-        // base64 sha256, so 44 bytes
-        signature: Type.String({ maxLength: 200 }),
-      }),
-  });
-  fastify.get(
-    "/",
-    {
-      schema: { querystring: querySchema },
-      config: {
-        rateLimit: <RateLimitOptions>{
-          hook: "preHandler",
-          max: (
-            _req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>,
-            key: string
-          ) => {
-            if (key?.startsWith?.("new")) {
-              return 5;
-            } else {
-              return 200;
-            }
-          },
-          keyGenerator: (
-            req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>
-          ) => {
-            return req.cached ? req.ip : `new${req.ip}`;
-          },
-        },
-      },
-      preParsing: [
-        (
-          req: FastifyRequest<{
-            Querystring: Static<typeof querySchema>;
-          }> & { originalUrl: string },
-          _res,
-          _payload,
-          done
-        ) => {
-          if (!req.query?.url) return done();
-          try {
-            req.query.url = decodeURIComponent(req.query.url);
-          } catch {
-            // do nothing (don't decode) if decode failed
-          }
-          req.originalUrl = req.query.url;
-          if (
-            !["https://", "http://"].some((v) => req.query.url?.startsWith(v))
-          ) {
-            req.query.url = `https://${req.query.url}`;
-          }
-          if (req.query.url?.endsWith("/")) {
-            req.query.url = req.query.url?.slice(0, -1);
-          }
-          req.query.url = req.query.url.split("?")[0];
-          done();
-        },
-      ],
-      preHandler: [
-        (
-          req: FastifyRequest<{ Querystring: Static<typeof querySchema> }> & {
-            originalUrl: string;
-          },
-          res,
-          done
-        ) => {
-          const { signature } = req.query;
-          const { originalUrl } = req;
-          if (config.HMAC_VERIFY && config.HMAC_KEY) {
-            if (!HMACVerify(config.HMAC_KEY, originalUrl, signature)) {
-              return res.code(403).send({
-                statusCode: 403,
-                error: "Forbidden",
-                message: "Invalid HMAC signature.",
-              });
-            }
-          }
-          done();
-        },
-        async (
-          req: FastifyRequest<{ Querystring: Static<typeof querySchema> }> & {
-            originalUrl: string;
-          },
-          res: FastifyReply
-        ) => {
-          const { url } = req.query;
-          if (!url) return;
-
-          try {
-            const ips: string[] = await new Promise((resolve, reject) => {
-              dns.resolve(new URL(url).hostname, (err, ips) => {
-                if (err) reject(err);
-                else resolve(ips);
-              });
-            });
-
-            if (
-              (await Promise.all(ips.map((ip) => isLocalhost(ip)))).some(
-                Boolean
-              )
-            ) {
-              return res.code(403).send({
-                statusCode: 403,
-                error: "Refused to process private or local address.",
-              });
-            }
-          } catch {
-            return res
-              .code(400)
-              .send({ statusCode: 400, error: "Failed to resolve hostname." });
-          }
-        },
-        async (
-          req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>
-        ) => {
-          if (req.query?.url) {
-            const cached = await redis
-              .get(genkey_redis(req.query.url))
-              .then((result) => Boolean(result))
-              .catch(() => false);
-            req.cached = cached;
-          }
-        },
-      ],
-    },
-    async (
-      req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>,
-      res
-    ) => {
-      const { url } = req.query;
-
-      const cache = (await cacheCl.findOne(
-        { url },
-        { projection: { _id: 0, metadata: 1 } }
-      )) as unknown as Cache | null;
-
-      const redis_key = genkey_redis(url);
-
-      if (cache) {
-        if (!req.cached) {
-          redis
-            .set(
-              redis_key,
-              1,
-              "EX",
-              Math.round(
-                positiveOrZero(
-                  new Date(cache.createdAt).getTime() +
-                    1000 * 60 * 60 * 24 * 30 -
-                    new Date().getTime()
-                ) / 1000
-              ) || 60 * 60 * 24 * 30
-            )
-            .catch(console.error);
-        }
-        return res.send({ metadata: cache.metadata });
-      }
-
-      if (req.cached && !cache) {
-        redis.del(redis_key).catch(console.error);
-      }
-
-      const data = await metadataScraper(url);
-      if (!data) {
-        res.send({ metadata: null });
-        await cacheCl
-          .insertOne({
-            url,
-            createdAt: new Date(),
-            metadata: null,
-          })
-          .catch(console.error);
-        await redis
-          .set(redis_key, 1, "EX", 60 * 60 * 24 * 1)
-          .catch(console.error);
-      } else {
-        const metadata = {
-          title: data.title ?? null,
-          description: data.description ?? null,
-          image: data.image ?? null,
-          ...(data.image &&
-            config.HMAC_SIGN &&
+    const querySchema = Type.Object({
+        url: Type.String({
+            pattern:
+                "^https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]{0,2000})$",
+            maxLength: 1000,
+        }),
+        ...(config.HMAC_VERIFY &&
             config.HMAC_KEY && {
-              image_signature: HMACSign(config.HMAC_KEY, data.image),
+                // base64 sha256, so 44 bytes
+                signature: Type.String({ maxLength: 200 }),
             }),
-          hostname: new URL(url).hostname ?? null,
-          siteName: data.publisher ?? null,
-        };
-        res.send(<{ metadata: APIResponse }>{
-          metadata,
-        });
-        await cacheCl
-          .insertOne({ url, createdAt: new Date(), metadata })
-          .catch(console.error);
-        await redis
-          .set(redis_key, 1, "EX", 60 * 60 * 24 * 30)
-          .catch(console.error);
-      }
-    }
-  );
-  done();
+    });
+    fastify.get(
+        "/",
+        {
+            schema: { querystring: querySchema },
+            config: {
+                rateLimit: <RateLimitOptions>{
+                    hook: "preHandler",
+                    max: (
+                        _req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>,
+                        key: string,
+                    ) => {
+                        if (key?.startsWith?.("new")) {
+                            return 5;
+                        } else {
+                            return 200;
+                        }
+                    },
+                    keyGenerator: (
+                        req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>,
+                    ) => {
+                        return req.cached ? req.ip : `new${req.ip}`;
+                    },
+                },
+            },
+            preParsing: [
+                (
+                    req: FastifyRequest<{
+                        Querystring: Static<typeof querySchema>;
+                        Body: { originalUrl?: string };
+                    }>,
+                    _res,
+                    _payload,
+                    done,
+                ) => {
+                    req.body = {};
+                    if (!req.query?.url) return done();
+                    try {
+                        req.query.url = decodeURIComponent(req.query.url);
+                    } catch {
+                        // do nothing (don't decode) if decode failed
+                    }
+
+                    req.body.originalUrl = req.query.url;
+                    if (
+                        !["https://", "http://"].some((v) => req.query.url?.startsWith(v))
+                    ) {
+                        req.query.url = `https://${req.query.url}`;
+                    }
+                    if (req.query.url?.endsWith("/")) {
+                        req.query.url = req.query.url?.slice(0, -1);
+                    }
+                    req.query.url = req.query.url.split("?")[0];
+                    done();
+                },
+            ],
+            preHandler: [
+                (
+                    req: FastifyRequest<{
+                        Querystring: Static<typeof querySchema>;
+                        Body: { originalUrl?: string };
+                    }>,
+                    res,
+                    done,
+                ) => {
+                    const { signature } = req.query;
+                    const { originalUrl } = req.body;
+                    if (config.HMAC_VERIFY && config.HMAC_KEY) {
+                        if (!HMACVerify(config.HMAC_KEY, originalUrl, signature)) {
+                            return res.code(403).send({
+                                statusCode: 403,
+                                error: "Forbidden",
+                                message: "Invalid HMAC signature.",
+                            });
+                        }
+                    }
+                    done();
+                },
+                async (
+                    req: FastifyRequest<{
+                        Querystring: Static<typeof querySchema>;
+                    }>,
+                    res: FastifyReply,
+                ) => {
+                    const { url } = req.query;
+                    if (!url) return;
+
+                    try {
+                        const ips: string[] = await new Promise((resolve, reject) => {
+                            dns.resolve(new URL(url).hostname, (err, ips) => {
+                                if (err) reject(err);
+                                else resolve(ips);
+                            });
+                        });
+
+                        if (
+                            (await Promise.all(ips.map((ip) => isLocalhost(ip)))).some(
+                                Boolean,
+                            )
+                        ) {
+                            return res.code(403).send({
+                                statusCode: 403,
+                                error: "Refused to process private or local address.",
+                            });
+                        }
+                    } catch {
+                        return res.code(400).send({
+                            statusCode: 400,
+                            error: "Failed to resolve hostname.",
+                        });
+                    }
+                },
+                async (
+                    req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>,
+                ) => {
+                    if (req.query?.url) {
+                        const cached = await redis
+                            .get(genkey_redis(req.query.url))
+                            .then((result) => Boolean(result))
+                            .catch(() => false);
+                        req.cached = cached;
+                    }
+                },
+            ],
+        },
+        async (req: FastifyRequest<{ Querystring: Static<typeof querySchema> }>, res) => {
+            const { url } = req.query;
+
+            const cache = (await cacheCl.findOne(
+                { url },
+                { projection: { _id: 0, metadata: 1 } },
+            )) as unknown as Cache | null;
+
+            const redis_key = genkey_redis(url);
+
+            if (cache) {
+                if (!req.cached) {
+                    redis
+                        .set(
+                            redis_key,
+                            1,
+                            "EX",
+                            Math.round(
+                                positiveOrZero(
+                                    new Date(cache.createdAt).getTime() +
+                                        1000 * 60 * 60 * 24 * 30 -
+                                        new Date().getTime(),
+                                ) / 1000,
+                            ) || 60 * 60 * 24 * 30,
+                        )
+                        .catch(console.error);
+                }
+                return res.send({ metadata: cache.metadata });
+            }
+
+            if (req.cached && !cache) {
+                redis.del(redis_key).catch(console.error);
+            }
+
+            const data = await metadataScraper(url);
+            if (!data) {
+                res.send({ metadata: null });
+                await cacheCl
+                    .insertOne({
+                        url,
+                        createdAt: new Date(),
+                        metadata: null,
+                    })
+                    .catch(console.error);
+                await redis
+                    .set(redis_key, 1, "EX", 60 * 60 * 24 * 1)
+                    .catch(console.error);
+            } else {
+                const metadata = {
+                    title: data.title ?? null,
+                    description: data.description ?? null,
+                    image: data.image ?? null,
+                    ...(data.image &&
+                        config.HMAC_SIGN &&
+                        config.HMAC_KEY && {
+                            image_signature: HMACSign(config.HMAC_KEY, data.image),
+                        }),
+                    hostname: new URL(url).hostname ?? null,
+                    siteName: data.publisher ?? null,
+                };
+                res.send(<{ metadata: APIResponse }>{
+                    metadata,
+                });
+                await cacheCl
+                    .insertOne({ url, createdAt: new Date(), metadata })
+                    .catch(console.error);
+                await redis
+                    .set(redis_key, 1, "EX", 60 * 60 * 24 * 30)
+                    .catch(console.error);
+            }
+        },
+    );
+    done();
 }
